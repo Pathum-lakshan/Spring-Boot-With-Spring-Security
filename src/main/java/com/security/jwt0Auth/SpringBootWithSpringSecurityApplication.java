31cc53ec306e07957631fdd755e5642b22d1dce6
Pathum Lakshan <pathuml@lassana.com>
2023-09-13 11:01:57 +0530

    initial commit

---
+++ src/main/java/com/security/jwt0Auth/SpringBootWithSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.security.jwt0Auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/security/jwt0Auth/SpringBootWithSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.jwt0Auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
