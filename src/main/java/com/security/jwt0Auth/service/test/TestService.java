a962e4f5f2aabca6d929a05cf69a2691501a998d
Pathum Lakshan <pathuml@lassana.com>
2023-09-14 20:36:56 +0530

    complete test get method with server servlet context parent path

---
+++ src/main/java/com/security/jwt0Auth/service/test/TestService.java
@@ -0,0 +1,13 @@
+package com.security.jwt0Auth.service.test;
+
+/**
+ * @Author : Pathum Lakshan
+ * @Project : Spring-Boot-With-Spring-Security
+ * @Date : 2023-09-14 8:01 PM
+ */
+
+public interface TestService {
+
+    String test();
+
+}
