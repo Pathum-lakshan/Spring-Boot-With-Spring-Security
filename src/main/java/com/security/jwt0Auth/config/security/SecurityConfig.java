2fbd3702591217385e2318dd25565c5c1ecf2f7b
Pathum Lakshan <pathuml@lassana.com>
2023-09-16 13:54:55 +0530

    complete implement

--- src/main/java/com/security/jwt0Auth/config/security/SecurityConfig.java
+++ src/main/java/com/security/jwt0Auth/config/security/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.security.jwt0Auth.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @Author : Pathum Lakshan
@@ -14,6 +21,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -24,5 +38,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("1234"))
+                .roles("ADMIN")
+                .build();
+        UserDetails writer = User.builder()
+                .username("writer")
+                .password(passwordEncoder.encode("1234"))
+                .roles("WRITE")
+                .build();
+        return new InMemoryUserDetailsManager(admin,writer);
     }
 }
